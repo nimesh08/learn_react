e6684bcb75cc1e5c9129d2fe3b6fefe827909565
Nimesh Cheedella <chnimesh0808@gmail.com>
2024-04-03 12:10:11 +0530

    baisc use of the key

--- new_concept/experiments/src/App.jsx
+++ new_concept/experiments/src/App.jsx
@@ -4,26 +4,70 @@ import './App.css'
 
 function App() {
 
-  const[text,setText]=useState("HELLO")
-  function upatetitle(){
-  setText(Math.random);
-         }
-  return (
-    <>
-      <button onClick={upatetitle}>click to do random change</button><br />
-      <Header title={text}></Header>
- <Header title="hiiiiii1111"></Header>
-    </>
-  )
+
+
+
+
+  
+
+//for learning key
+// const [todos,setTodo]=useState([{id:1,title:'hiiii',description:'bue'},{id:2,title:'hiiii2',description:'bue2'},{id:3,title:'hiiii3',description:'bue3'}]);
+
+// function addTodo(){
+// setTodo([...todos,{
+//   id:4,
+//   title:Math.random(),
+//   description:Math.random()
+// }])
+
+
+// }
+// console.log(todos);
+// return (
+//   <div>
+//     <button onClick={addTodo}>add todo</button>
+//     {todos.map(todo => (
+//       <Todo key={todo.id} title={todo.title} description={todo.description} />
+//     ))}
+//   </div>
+// );
 }
+
+
+
+
+
+
+
+//   const[text,setText]=useState("HELLO")
+//   function upatetitle(){
+//   setText(Math.random);
+//          }
+//   return (
+//     <>
+//       <button onClick={upatetitle}>click to do random change</button><br />
+//       <Header title={text}></Header>
+//  <Header title="hiiiiii1111"></Header>
+//     </>
+//   )
+
+
+// eslint-disable-next-line react/prop-types
+// const Header=memo(function Header({title}){
+//   return <div>
+//     {title}
+//   </div>
+//   })
 
 // eslint-disable-next-line react/prop-types
 
 
-const Header=memo(function Header({title}){
-  return <div>
-    {title}
-  </div>
-  })
+// for learning key
+// function Todo({title,description}){
+//   return <div>
+//     <h1>{title}</h1>
+//     <h5>{description}</h5>
+//   </div>
+// }
 
 export default App
